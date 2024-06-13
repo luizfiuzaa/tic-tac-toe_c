1cb47c9fc9f22bd83bbe685fd23117473159b57c
luizfiuzaa <luizffreitas2006@gmail.com>
2024-06-13 17:53:31 -0300

    $

--- main.c
+++ main.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LINHA 3
+#define COLUNA 3
+
+// VISUAL
 int print_line(){
-    printf("\n-----------------------------------------------\n");
+    printf("-----------------------------------------------\n");
     return 0;
 }
 
 int menu_escolha_modo_jogo(){
     print_line();
-    printf("\t\t JOGO DA VELHA");
+    printf("\t\t JOGO DA VELHA \n");
     print_line();
-    printf(" MODO DE JOGO");
+    printf(" MODO DE JOGO \n");
     print_line();
     printf(" 1 - Humano x Humano \n");
     printf(" 2 - Humano x Maquina \n");
     printf(" 3 - Maquina x Maquina \n");
-    printf(" 0 - Sair");
+    printf(" 0 - Sair \n");
     print_line();
     return 0;
 }
@@ -38,9 +42,38 @@ int escolha_modo_jogo(){
     return modo;
 }
 
+int ganhou(int rodadas){
+    if()
+}
+
+// JOGO
+
+int jogo_partida(){
+    int modo;
+    int rodadas;
+    escolha_modo_jogo();
+    char matriz[LINHA][COLUNA];
+    
+        if(modo == 1){
+            humano_contra_humano();
+        }
+    
+    return 0;
+}
+
+int humano_contra_humano(){
+
+}
+
+
+
+
+
 int main(){
     int modo;
+    
     menu_escolha_modo_jogo();
-    escolha_modo_jogo();
-    return modo;
+
+    
+    return 0;
 }
